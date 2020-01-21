ce02abed9885c18af8589432cfdf89720e7c20d0
Hunter <hunterbe1998@gmail.com>
2020-01-21 15:40:39 -0500

    Validating IDs and Regexs

--- Compiler1/Compiler1/Grammar.js
+++ Compiler1/Compiler1/Grammar.js
@@ -1,12 +1,11 @@
-export class Grammar {
-
-    constructor(input: string) {
-        
-        let id: Set<string> = new Set();
-        let rex: Set<string> = new Set();
+"use strict";
+exports.__esModule = true;
+var Grammar = /** @class */ (function () {
+    function Grammar(input) {
+        var id = new Set();
+        var rex = new Set();
         var grams = input.split("\n");
         for (var i = 0; i < grams.length - 1; i++) {
-            
             if (grams[i].length == 0) {
                 continue;
             }
@@ -14,7 +13,6 @@ export class Grammar {
                 throw new Error("no junction ");
             }
             var g = grams[i].split(" -> ");
-
             if (id.has(g[0])) {
                 throw new Error("ID already created.");
             }
@@ -27,18 +25,15 @@ export class Grammar {
             else if (g[1] == "") {
                 throw new Error("Empty ID");
             }
-         
             try {
                 new RegExp(g[1]);
             }
-            catch {
+            catch (_a) {
                 throw new Error("Invalid Regex");
             }
-            
             id.add(g[0]);
         }
-
-        
-
     }
-}
+    return Grammar;
+}());
+exports.Grammar = Grammar;
